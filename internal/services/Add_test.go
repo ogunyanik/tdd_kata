97caddb5c40346a325408981d9a346f40b16d39b
ogunyanik <ogunyanik@gmail.com>
2022-06-21 16:02:39 +0300

    Case4 test cases added (status=red)

--- internal/services/Add_test.go
+++ internal/services/Add_test.go
@@ -11,10 +11,12 @@ func Test_Add(t *testing.T) {
 	case1, _ := Add("1,2")
 	case2, _ := Add("1\n2,3")
 	case3, err_case3 := Add("1,\n")
+	case4, _ := Add("//;\n1;2")
 
 	assert.Equal(t, 3, case1)
 	assert.Equal(t, 6, case2)
 	assert.Equal(t, 0, case3)
 	assert.Equal(t, "not valid", err_case3.Error())
+	assert.Equal(t, 3, case4)
 
 }
