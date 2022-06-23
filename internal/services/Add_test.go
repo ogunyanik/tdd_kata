aab919267ba066aafc577004fa7e123b6557904d
ogunyanik <ogunyanik@gmail.com>
2022-06-23 10:42:26 +0300

    case5 v2 added (status=red)

--- internal/services/Add_test.go
+++ internal/services/Add_test.go
@@ -13,6 +13,7 @@ func Test_Add(t *testing.T) {
 	case3, err_case3 := Add("1,\n")
 	case4, _ := Add("//;\n1;2")
 	_, err5 := Add("-1,2")
+	_, err5_v2 := Add("-1,-2")
 
 	assert.Equal(t, 3, case1)
 	assert.Equal(t, 6, case2)
@@ -20,5 +21,6 @@ func Test_Add(t *testing.T) {
 	assert.Equal(t, "not valid", err_case3.Error())
 	assert.Equal(t, 3, case4)
 	assert.Equal(t, "negatives not allowed", err5.Error())
+	assert.Equal(t, "negatives not allowed!! negative numbers: (-1,-2)", err5_v2.Error())
 
 }
