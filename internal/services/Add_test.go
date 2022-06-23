aa2428af719ec2d30fbe63e7ca58e0ca0f52f4f6
ogunyanik <ogunyanik@gmail.com>
2022-06-23 10:33:50 +0300

    new test case5 negative numbers error (status=red)

--- internal/services/Add_test.go
+++ internal/services/Add_test.go
@@ -12,11 +12,13 @@ func Test_Add(t *testing.T) {
 	case2, _ := Add("1\n2,3")
 	case3, err_case3 := Add("1,\n")
 	case4, _ := Add("//;\n1;2")
+	_, err5 := Add("-1,2")
 
 	assert.Equal(t, 3, case1)
 	assert.Equal(t, 6, case2)
 	assert.Equal(t, 0, case3)
 	assert.Equal(t, "not valid", err_case3.Error())
 	assert.Equal(t, 3, case4)
+	assert.Equal(t, "negatives not allowed", err5)
 
 }
